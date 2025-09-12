61a18541323a085fb6b5c252edaa61a5b01d7147
ahmed <ahmed@AHMED_SAMY>
2025-09-12 17:16:31 +0300

    Part 03

---
+++ S1/Context/CompanyG01DBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace S1.Context
+{
+    internal class CompanyG01DBContext : DbContext
+    {
+
+        public CompanyG01DBContext() : base()
+        {
+               
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Data Source = .;intial Catalog = CompanyG01;integraded Security = true");
+
+            optionsBuilder.UseSqlServer("Server = .;Database = CompanyG01;Trusted_Connection = True");
+
+        }
+
+    }
+}
--- S1/Program.cs
+++ S1/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace S1
+﻿using S1.Context;
+namespace S1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //CompanyG01DBContext dbContext = new CompanyG01DBContext();
+            //try
+            //{
+            //    //Some Code
+            //}
+            //finally
+            //{
+            //    dbContext.Dispose();
+            //}
+
+            //using(CompanyG01DBContext dbContext = new CompanyG01DBContext())
+            //{
+            //    // some code
+            //}
+
+            using CompanyG01DBContext dbContext = new CompanyG01DBContext();
         }
     }
 }
